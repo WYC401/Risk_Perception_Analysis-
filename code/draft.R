5cd1ab1249c44c82b8bb702d0f7142dc842344ff
wyc <545301030@qq.com>
2022-04-04 23:17:19 -0700

    find the way to organize the data

--- code/draft.R
+++ code/draft.R
@@ -1,5 +1,6 @@
 library(here)
 library(stringr)
+library(dplyr)
 data <- load(here::here("raw_data/moduletwo.rda"))
 l <- moduletwo[1,]
 nameOfQuestion <- names(l)
@@ -7,3 +8,10 @@ dependentVariablesNames <- c(
                             nameOfQuestion[str_detect(nameOfQuestion, regex("Ben", ignore_case= TRUE))],
                             nameOfQuestion[str_detect(nameOfQuestion, regex("Risky", ignore_case= TRUE))]
                             )
+KehenScaleNames <- nameOfQuestion[str_detect(nameOfQuestion, regex("K_", ignore_case= TRUE))]
+likert <- c("Rather not say/ Don't know", )
+dataUsed %>% sapply(function(x) { mapvalues(x,
+                                            c("Rather not say/ Don't know"),
+                                            1)})
+r <- nrow(moduletwo)
+dataUsed <- moduletwo[3:r,]
